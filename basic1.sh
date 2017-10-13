16f33b3a40704a4abdb04ee50a03169a3dfd4098
uppiupender <uppi006@gmail.com>
2017-10-13 15:28:23 -0400

    adding new script

---
+++ basic1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Biginner script
+
+echo "Please enter your First name : "  # Prints Please enter your First name
+read FIRST        			# Storing first name into a variable called FIRST
+
+echo "Please enter your secondname : "  # Prints Please enter your secondname
+read SECOND				# storing second name into a variable called SECOND
+
+# now Calling that variables
+clear                  			# clears the screen
+echo "Hello"
+echo "Good morning $FIRST $SECOND"   # calling variable (use $ to call a varibale)
+
